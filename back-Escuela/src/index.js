8aa508a2c7b42892ee758c376d164786d6030317
Dev-Simon20 <jeanpierks6@gmail.com>
2023-12-02 10:41:45 -0500

    devuelve un res send en la ruta raiz

---
+++ back-Escuela/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const routes = require('./api/endPoints')
+const cors = require('cors');
+
+app.use(express.json());
+
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST"]
+}));
+
+app.use('/', routes);
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
